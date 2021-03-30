b5b39ff030c9a8a7882a4f4773c51de5b2331878
suibiweng <suibi.weng@gmail.com>
2021-03-31 01:39:40 +0800

    0331

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+  public  float speedX;
+  public  float speedY;
+  public Transform barA;
+  public Transform barB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedX = 0.03f;
+        speedY = 0.03f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(speedX, speedY, 0));
+
+        if (transform.position.x > 9.0f) {
+            speedX = speedX * -1;
+        }
+        if (transform.position.x < -9.0f)
+        {
+            speedX = speedX * -1;
+        }
+        if (transform.position.y > 5f)
+        {
+            speedY = speedY * -1;
+        }
+        if (transform.position.y < -5f)
+        {
+            speedY = speedY * -1;
+        }
+        if ( transform.position.x+0.5F>= barA.position.x) {
+
+            if (transform.position.y > barA.transform.position.y - barA.localScale.y / 2 && transform.position.y < barA.transform.position.y + barA.localScale.y / 2) {
+
+                speedX = speedX * -1;
+            }
+            
+
+        }
+        if (transform.position.x  <= barB.position.x)
+        {
+            if (transform.position.y > barB.transform.position.y - barB.localScale.y / 2 && transform.position.y < barB.transform.position.y + barB.localScale.y / 2)
+            { 
+                speedX = speedX * -1;
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    public float speed;
+    public bool isA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isA)
+        {
+
+            if (Input.GetKey(KeyCode.O))
+            {
+                transform.Translate(new Vector3(0, speed, 0));
+
+            }
+
+            if (Input.GetKey(KeyCode.L))
+            {
+                transform.Translate(new Vector3(0, -speed, 0));
+            }
+        }
+        else {
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0, speed, 0));
+
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, -speed, 0));
+            }
+        }
+
+    }
+}
